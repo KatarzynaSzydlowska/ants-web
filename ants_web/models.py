8264152148f11c7a4418ef14967250e726c7d434
Krzysztof Szydłowski <kriszna14@gmail.com>
2016-11-22 11:50:48 +0100

    model & db migration

--- ants_web/models.py
+++ ants_web/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __init__(self, name, *args, **kwargs):
+        super(Course, self).__init__(*args, **kwargs)
+        self.name = name
--- ants_web/tests.py
+++ ants_web/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from ants_web.models import Course
 
-# Create your tests here.
+class CourseTestCase(TestCase):
+    def test_course_can_be_created(self):
+        course = Course('Python in the enterprise')
+        self.assertEqual(course.name, 'Python in the enterprise')
